417b23c20728960b8828e9f7b3a190d7249a8a6d
TowelToaster <d.landgraf2000@icloud.com>
2024-11-27 21:09:11 +0100

    Disabel zoom on mobile #36

--- src/main.ts
+++ src/main.ts
@@ -9,5 +9,11 @@ import { AppComponent } from './app/app.component';
 
 initEffects();
 
+// when the user rotates the device from landscape to portait the zoom sometimes breaks, reload the page to fix that
+// as of now this only occurs on iOS and interestingly ONLY when the PWA is installed, but NOT if it is used as a normal website in Safari
+window.addEventListener('orientationchange', () => {
+  location.reload();
+});
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
