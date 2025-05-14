d76dcd4a5f2600c2b27a598918fc9ff6aaef7505
daniel2202x <d.landgraf2000@icloud.com>
2025-05-14 17:47:54 +0000

    fix lint errors

--- src/app/directives/icon.component.ts
+++ src/app/directives/icon.component.ts
@@ -1,29 +1,29 @@
 import { Component, computed, input } from '@angular/core';
 
 @Component({
-  selector: '[app-icon]',
+  selector: '[appIcon]',
   standalone: true,
   imports: [],
-  template: `<i [class]="'bi bi-' + icon()"></i> <ng-content></ng-content>`,
+  template: `<i [class]="'bi bi-' + appIcon()"></i> <ng-content></ng-content>`,
   host: {
     '[class]': 'classes()'
   }
 })
 export class IconComponent {
 
-  readonly icon = input.required<string>({ alias: 'app-icon' });
-  readonly colour = input<'success' | 'primary' | 'secondary' | 'danger' | 'link' | 'dark' | null>(null, { alias: 'app-icon-colour' });
-  readonly size = input<'lg' | 'sm' | null>(null, { alias: 'app-icon-size' });
+  readonly appIcon = input.required<string>();
+  readonly appIconColour = input<'success' | 'primary' | 'secondary' | 'danger' | 'link' | 'dark' | null>(null);
+  readonly appIconSize = input<'lg' | 'sm' | null>(null);
 
   readonly classes = computed(() => {
     let classes = 'btn';
 
-    if (this.colour()) {
-      classes += ` btn-${this.colour()}`;
+    if (this.appIconColour()) {
+      classes += ` btn-${this.appIconColour()}`;
     }
 
-    if (this.size()) {
-      classes += ` btn-${this.size()}`;
+    if (this.appIconSize()) {
+      classes += ` btn-${this.appIconSize()}`;
     }
 
     return classes;
